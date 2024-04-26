a5fa90783befc234facd247a7f63f44a9ba1208d
arijghrs <arijgharsalli8@gmail.com>
2024-04-26 17:22:22 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+
 
 
 const app = express();
+
+const connect = async () => {
+    try {
+     await mongoose.connect(process.env.MONGO);
+     console.log("connected to mongo")
+    } catch (error) {
+     throw error;
+    }}
+    mongoose.connection.on("disconnected", ()=>{
+      console.log("mongoDB disconnected")
+    })
+    mongoose.connection.on("connected", ()=>{
+      console.log("mongoDB connected")
+    })
 
 
 app.listen(3000, () =>{
