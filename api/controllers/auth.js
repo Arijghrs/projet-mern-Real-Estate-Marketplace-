130f1bd0f504261199bbe233dfaccdc68ec2741a
arijghrs <arijgharsalli8@gmail.com>
2024-04-28 16:41:10 +0100

    ccreate middleware to handleerrors

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -2,14 +2,13 @@ import User from '../models/user.js';
 import bcryptjs from 'bcryptjs';
 
 
-export const signup = async (req,res) =>{
-    try {
+export const signup = async (req,res,next) =>{
     const {username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username,email,password: hashedPassword});
+    try {
     await newUser.save();
     res.status(201).json("user created")
 } catch (error) {
-    console.error("Error creating user:", error);
-    res.status(500).json({ error: "An internal server error occurred" });
+    next(error);
 }};
